29d4907c28be7a2a16b3a3a635c33d1888b11fcd
Mihail Rotaru <355401@via.dk>
2026-04-29 18:28:26 +0200

    Fixed base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Change '/pokedex/' to match your GitHub repository name
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/Pokedex/',
 })
